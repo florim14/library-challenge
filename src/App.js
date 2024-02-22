bcab189cb1dab3c8c13dad00518a29d6fae1e66b
hamiti <florim.hamiti@fit.fraunhofer.de>
2024-02-23 00:43:16 +0100

    Modified the routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react"
-import { Route, Routes } from "react-router"
-import { BrowserRouter } from "react-router-dom"
 import Container from '@mui/material/Container'
 import Home from "./components/Home"
 
 export default function App() {
 
-  return <BrowserRouter>
-      <Container maxWidth={"xl"} sx={{ marginTop: 2 }}>
-          <Routes>
-              <Route path={"/*"} element={<Home />}/>
-          </Routes>
-      </Container>
-  </BrowserRouter>
+  return <Container maxWidth={"xl"} sx={{ marginTop: 2 }}>
+      <Home />
+  </Container>
 }
